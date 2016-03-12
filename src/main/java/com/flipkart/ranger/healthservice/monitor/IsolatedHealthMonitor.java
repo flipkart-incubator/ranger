230c19817303342135c5667eb6f05beb15712315
TusharNaik <tushar.naik@flipkart.com>
2016-03-12 23:45:50 +0530

    adding licence to all newly created files

--- src/main/java/com/flipkart/ranger/healthservice/monitor/IsolatedHealthMonitor.java
+++ src/main/java/com/flipkart/ranger/healthservice/monitor/IsolatedHealthMonitor.java
@@ -1,6 +1,21 @@
+/**
+ * Copyright 2016 Flipkart Internet Pvt. Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.flipkart.ranger.healthservice.monitor;
 
-import com.flipkart.ranger.healthcheck.HealthcheckStatus;
 import com.flipkart.ranger.healthservice.TimeEntity;
 
 import java.util.Date;
@@ -8,7 +23,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
- * A health monitor that implements a {@link Monitor<HealthcheckStatus>} and {@link Runnable}
+ * A health monitor that implements a {@link Monitor<T>} and {@link Runnable}
  * Maintains the latest state of the Monitor, and its latest updated timestamp
  *
  * @see com.flipkart.ranger.healthservice.monitor.sample.PingCheckMonitor
