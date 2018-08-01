2c90b0a4626852721a8eb949fb5580ac32dda9db
Mudit G <mudit.g@PP-C02WK0HLG8WN.local>
2018-08-01 16:40:34 +0530

    corrected ServiceHealthAggregatorTest

--- src/test/java/com/flipkart/ranger/healthservice/ServiceHealthAggregatorTest.java
+++ src/test/java/com/flipkart/ranger/healthservice/ServiceHealthAggregatorTest.java
@@ -23,11 +23,14 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ServiceHealthAggregatorTest {
-
+    /* Logger */
+    private static final Logger logger = LoggerFactory.getLogger(ServiceHealthAggregatorTest.class);
 
     ServiceHealthAggregator serviceHealthAggregator = new ServiceHealthAggregator();
     TestMonitor testMonitor;
@@ -59,14 +62,15 @@ public class ServiceHealthAggregatorTest {
     @Test
     public void testStaleRun() throws Exception {
         testMonitor.run();
-        testMonitor.setThreadSleep(3000);
+        testMonitor.setThreadSleep(6000);
 
         Thread.sleep(4000);
 
-        /* in the TestMonitor, thread was sleeping for 3 seconds, */
+        /* in the TestMonitor, thread was sleeping for 5 seconds, */
         /* so its state is supposed to be stale (>1 second) and service has to be unhealthy */
         Assert.assertEquals(HealthcheckStatus.unhealthy, serviceHealthAggregator.getServiceHealth());
 
+//        Thread.sleep(4000);
 
         testMonitor.setThreadSleep(10);
         Thread.sleep(4000);
