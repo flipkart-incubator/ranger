7210e86f00223542092f55e2685484a77488eaf2
Koushik R <rkoushik.14@gmail.com>
2021-09-24 15:03:15 +0530

    Fixed HttpShardedServiceFinderBuilderTest

--- ranger-http/src/test/java/com/flipkart/ranger/http/HttpShardedServiceFinderBuilderTest.java
+++ ranger-http/src/test/java/com/flipkart/ranger/http/HttpShardedServiceFinderBuilderTest.java
@@ -9,7 +9,6 @@ import com.flipkart.ranger.core.model.ServiceNode;
 import com.flipkart.ranger.core.utils.TestUtils;
 import com.flipkart.ranger.http.config.HttpClientConfig;
 import com.flipkart.ranger.http.model.ServiceNodesResponse;
-import com.flipkart.ranger.http.serde.HTTPResponseDataDeserializer;
 import com.flipkart.ranger.http.servicefinder.HttpShardedServiceFinderBuilder;
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import lombok.Data;
@@ -46,50 +45,52 @@ public class HttpShardedServiceFinderBuilderTest {
 
     @Test
     public void testFinder() throws Exception {
-//        final NodeData testNode = new NodeData("testNode");
-//        final ServiceNode<NodeData> node = new ServiceNode<>("127.0.0.1", 80, testNode);
-//        node.setHealthcheckStatus(HealthcheckStatus.healthy);
-//        node.setLastUpdatedTimeStamp(System.currentTimeMillis());
-//        val payload = MAPPER.writeValueAsBytes(
-//                ServiceNodesResponse.<NodeData>builder()
-//                        .node(node)
-//                        .success(true)
-//                        .build());
-//        server.stubFor(get(urlEqualTo("/ranger/nodes/v1/testns/test"))
-//                               .willReturn(aResponse()
-//                                                   .withBody(payload)
-//                                                   .withStatus(200)));
-//        val clientConfig = HttpClientConfig.builder()
-//                .host("127.0.0.1")
-//                .port(server.port())
-//                .connectionTimeoutMs(30_000)
-//                .operationTimeoutMs(30_000)
-//                .build();
-//        final SimpleShardedServiceFinder<NodeData> finder = new HttpShardedServiceFinderBuilder<NodeData, HTTPResponseDataDeserializer<NodeData>>()
-//                .withClientConfig(clientConfig)
-//                .withNamespace("testns")
-//                .withServiceName("test")
-//                .withDeserializer(data -> {
-//                    final ServiceNodesResponse<NodeData> response;
-//                    try {
-//                        response = MAPPER.readValue(data, new TypeReference<ServiceNodesResponse<NodeData>>() {});
-//                    }
-//                    catch (IOException e) {
-//                        throw new IllegalArgumentException(e);
-//                    }
-//                    return response.isSuccess() ? response.getNodes() : Collections.emptyList();
-//                })
-//                .withShardSelector((criteria, registry) -> registry
-//                        .nodes()
-//                        .entries()
-//                        .stream()
-//                        .filter(e -> e.getKey().getName().equals(criteria.getName()))
-//                        .map(Map.Entry::getValue)
-//                        .collect(Collectors.toList()))
-//                .build();
-//        finder.start();
-//        TestUtils.sleepForSeconds(3);
-//        Assert.assertNotNull(finder.get(testNode));
+        final NodeData testNode = new NodeData("testNode");
+        final ServiceNode<NodeData> node = new ServiceNode<>("127.0.0.1", 80, testNode);
+        node.setHealthcheckStatus(HealthcheckStatus.healthy);
+        node.setLastUpdatedTimeStamp(System.currentTimeMillis());
+        val payload = MAPPER.writeValueAsBytes(
+                ServiceNodesResponse.<NodeData>builder()
+                        .node(node)
+                        .success(true)
+                        .build());
+        server.stubFor(get(urlEqualTo("/ranger/nodes/v1/testns/test"))
+                               .willReturn(aResponse()
+                                                   .withBody(payload)
+                                                   .withStatus(200)));
+        val clientConfig = HttpClientConfig.builder()
+                .host("127.0.0.1")
+                .port(server.port())
+                .connectionTimeoutMs(30_000)
+                .operationTimeoutMs(30_000)
+                .build();
+
+        final SimpleShardedServiceFinder<NodeData> finder = new HttpShardedServiceFinderBuilder<NodeData>()
+                .withClientConfig(clientConfig)
+                .withNamespace("testns")
+                .withServiceName("test")
+                .withObjectMapper(MAPPER)
+                .withDeserializer(data -> {
+                    final ServiceNodesResponse<NodeData> response;
+                    try {
+                        response = MAPPER.readValue(data, new TypeReference<ServiceNodesResponse<NodeData>>() {});
+                    }
+                    catch (IOException e) {
+                        throw new IllegalArgumentException(e);
+                    }
+                    return response.isSuccess() ? response.getNodes() : Collections.emptyList();
+                })
+                .withShardSelector((criteria, registry) -> registry
+                        .nodes()
+                        .entries()
+                        .stream()
+                        .filter(e -> e.getKey().getName().equals(criteria.getName()))
+                        .map(Map.Entry::getValue)
+                        .collect(Collectors.toList()))
+                .build();
+        finder.start();
+        TestUtils.sleepForSeconds(3);
+        Assert.assertNotNull(finder.get(testNode));
     }
 
 }
