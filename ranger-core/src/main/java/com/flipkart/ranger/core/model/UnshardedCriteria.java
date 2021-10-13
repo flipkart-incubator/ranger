3806e9facb29e9f5115c7f009161642b3afa7957
Koushik R <rkoushik.14@gmail.com>
2021-10-13 22:17:07 +0530

    Made Criteria as interfaces instead. @FunctionalInterface at that

--- ranger-core/src/main/java/com/flipkart/ranger/core/model/UnshardedCriteria.java
+++ ranger-core/src/main/java/com/flipkart/ranger/core/model/UnshardedCriteria.java
@@ -2,8 +2,9 @@ package com.flipkart.ranger.core.model;
 
 import com.flipkart.ranger.core.finder.serviceregistry.ListBasedServiceRegistry;
 
-public abstract class UnshardedCriteria<T> implements Criteria<T, ListBasedServiceRegistry<T>> {
+@FunctionalInterface
+public interface UnshardedCriteria<T> extends Criteria<T, ListBasedServiceRegistry<T>> {
 
-    public abstract boolean apply(T nodeData);
+    boolean apply(T nodeData);
 
 }
